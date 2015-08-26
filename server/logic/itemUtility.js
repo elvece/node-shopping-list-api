bbfee369b5a98276cf4eaff70937b9284fcb7dba
elvece <lvcifferello@gmail.com>
2015-08-26 14:34:49 -0600

    added utility filter function, get single item request handled

--- server/logic/itemUtility.js
+++ server/logic/itemUtility.js
@@ -1,9 +1,61 @@
 var itemData = require('../models/items');
+var storageItemsArr = itemData.storage.items;
 
+//filter by id helper method, returns item if there, message if not
+function itemFilter(itemID){
+  var filteredItem = storageItemsArr.filter(function(item){
+    return itemID === item.id;
+    });
+  if (filteredItem.length === 0) {
+    return {message: 'Item does not exist in this list'};
+  } else {
+      return filteredItem;
+    }
 
+}
+
+function postHandler(name){
+  //if item is already in list, message saying so
+  //else, add item
+
+}
+
+//if item already exists, return message that item was successfully updated and return that updated item
+//if item is not in list, add it to list and return message that it was successfully added
+function putHandler(itemID, itemName){
+  if (storageItemsArr.id === itemID) {
+    storageItemsArr.name = itemName;
+    return {
+      updatedItem: storageItemsArr.name,
+      message: 'Item was successfully updated in the shopping list.'
+    };
+  }
+  else {
+    return {
+      addedItem: itemData.storage.addItem(itemName),
+      message: 'Item successfully added to the shopping list.'
+    };
+  }
+}
+
+//other put attempts
+//if the id in items array in storage is equal to the params id, then update the name
+  // for (var i = 0; i < storageItemsArr.length; i++) {
+  //   if (storageItemsArr[i].id === req.params.id){
+  //       storageItemsArr[i].name = name;
+  //     }
+  //     return {message: };
+
+  //   else {
+  //     itemData.storage.addItem(name);
+  //     message: 'Item successfully added to the shopping list.'
+  //   }
+  // }
 
 
 
 module.exports = {
-
+  storageItemsArr: storageItemsArr,
+  itemFilter: itemFilter,
+  putHandler: putHandler
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// var utility = require('../logic/itemUtility');
-// var allItems = require('../models/items');
+var utility = require('../logic/itemUtility');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Shopping List' });
@@ -9,44 +8,26 @@ router.get('/', function(req, res, next) {
 
 //GET route handler - all items
 router.get('/items', function(req, res) {
-  res.json(storage.items);
+  res.json(utility.storageItemsArr);
 });
 
-//POST route handler -
-router.post('/items', function(req, res) {
-  // add code here to create new item instance
-  storage.addItem(req.body.name);
-  res.send(storage);
-  console.log(storage);
+//GET route handler - single item by id
+router.get('/items/:id', function(req, res){
+  var response = utility.itemFilter(+req.params.id);
+  res.json(response);
 });
 
+//POST route handler -n
+router.post('/items', function(req, res) {//edit to include message if item already in list
+  //create new item instance
+  utility.itemData.storage.addItem(req.body.name);
+  res.send(utility.itemData.storage);
+});
 
-
-
-// Constructor
-function ItemLibrary() {
-  this.items = [];
-  this.id = 0;
-}
-
-// Methods
-ItemLibrary.prototype.addItem = function(name) {
-  var newItem = {
-    name: name,
-    id: this.id
-  };
-  this.id += 1;
-  this.items.push(newItem);
-};
-
-// Instances
-var storage = new ItemLibrary();
-storage.addItem('Noodles');
-storage.addItem('Tomatoes');
-storage.addItem('Peppers');
-
-// console.log(storage);
-
-
+//PUT route handler
+router.put('/items/:id', function(req, res){
+  var response = utility.putHandler(req.params.id, req.params.name);
+  res.json(response);
+});
 
 module.exports = router;
